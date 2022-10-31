f6e827d2a7aa801a37068ced35eb27cb4d360c50
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2022-10-31 16:25:03 +0100

    feat(mozaic): add mozaic config

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+const pluginList = require("@mozaic-ds/css-dev-tools/postcssPluginConfig");
+
+module.exports = {
+  plugins: pluginList,
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import includePaths from "@mozaic-ds/css-dev-tools/sassConfig";
+import pluginList from "@mozaic-ds/css-dev-tools/postcssPluginConfig";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: pluginList,
+    },
+    preprocessorOptions: {
+      scss: {
+        includePaths: [
+          "./node_modules/@mozaic-ds/styles/",
+          "./node_modules/@mozaic-ds/styles/components/",
+          "./node_modules/@mozaic-ds/styles/generic/",
+          "./node_modules/@mozaic-ds/styles/layouts/",
+          "./node_modules/@mozaic-ds/styles/settings-tools/",
+          "./node_modules/@mozaic-ds/styles/typography/",
+          "./node_modules/@mozaic-ds/styles/utilities/",
+          "./node_modules/",
+        ],
+      },
+    },
+  },
+});
