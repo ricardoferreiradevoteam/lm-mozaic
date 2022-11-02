7d045efcd3234d0bbed9e25780eb43559fb9fcb4
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2022-11-02 10:55:07 +0100

    feat(mozaic): add config and minimum example

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import vue from '@vitejs/plugin-vue';
 import includePaths from "@mozaic-ds/css-dev-tools/sassConfig";
 import pluginList from "@mozaic-ds/css-dev-tools/postcssPluginConfig";
 
+const plugins = pluginList.filter(
+  ({ postcssPlugin }) => postcssPlugin !== "stylelint"
+);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+  },
   css: {
     postcss: {
-      plugins: pluginList,
+      plugins: plugins,
     },
     preprocessorOptions: {
       scss: {
@@ -20,6 +27,8 @@ export default defineConfig({
           "./node_modules/@mozaic-ds/styles/settings-tools/",
           "./node_modules/@mozaic-ds/styles/typography/",
           "./node_modules/@mozaic-ds/styles/utilities/",
+          "./node_modules/@mozaic-ds/tokens/build/css/",
+          "./node_modules/@mozaic-ds/tokens/build/scss/",
           "./node_modules/",
         ],
       },
